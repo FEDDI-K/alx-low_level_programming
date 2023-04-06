afccc70303e1bf3a6ca3ac49efa2e70ca7197ed9
FEDDI-K <feddika@gmail.com>
2023-04-05 18:07:59 -0700

    100-is_palindrome correction

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -18,7 +18,8 @@ int is_palindrome(char *s)
 
 /**
  * _strlen_recursion - returns the length of a string
- * @s: string to calculate the length 
+ * @s: string to calculate the length of
+ *
  * Return: length of the string
  */
 int _strlen_recursion(char *s)
@@ -33,6 +34,7 @@ int _strlen_recursion(char *s)
  * @s: string to check
  * @i: iterator
  * @len: length of the string
+ *
  * Return: 1 if palindrome, 0 if not
  */
 int check_pal(char *s, int i, int len)
